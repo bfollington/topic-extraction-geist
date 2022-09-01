bf58699f53548214cc74c5ce17ba24e8ae693603
Ben Follington <5009316+bfollington@users.noreply.github.com>
2022-09-01 12:37:14 +1000

    Add more notes

--- src/notes.ts
+++ src/notes.ts
@@ -17,6 +17,54 @@ Evolutionary systems often generate unexpected solutions. Nature selects for goo
     author: "gordonbrander",
   },
   {
+    title: "Requisite Variety",
+    body: `Ashby’s Law of Requisite Variety:
+
+  > If a system is to be stable, the number of states of its control mechanism must be greater than or equal to the number of states in the system being controlled.
+  
+  Or:
+  
+  Only variety can absorb variety.
+  
+  > Variety is a measure of the number of distinct states a system can be in. For a system to control another system, it needs to have at least as many distinct states as the system being controlled. This is called requisite variety.
+  
+  Why?
+  
+  > The amount of output from a system is limited by the variety possible within the system and/or the variety of input to the system. The number of possible alternative communications between the two systems is limited by that system having the fewest output alternatives and/or the fewest input alternatives.
+  > Charles E. Osgood
+  
+  This is more than a design pattern, it is a generalizable law.
+  
+  ## Two ways to achieve variety
+
+  There are two ways to achieve requisite variety:
+  
+  - The controlling system increases variety to exceed the system being controlled.
+  - The controlling system limits the variety of the system being controlled.
+
+  ## Variety must be embodied
+
+  Knowledge in a system is embodied in the structure of that system. Therefore, the complexity of an adaptive system must match the complexity of that system’s environment.
+  
+  > If you want to make sense of a complex world, you've got to have an internal system that is equally complex.
+  > Karl Weick
+  
+  ## Limiting variety is authoritarian
+  To be authoritarian is to choose methods of control that limit variety.
+  
+  Any ideology that flattens the complexity of human life down to a simple system will embody less variety than the reality of human experience.
+  
+  ## Static systems must limit variety to remain stable
+  The only way to maintain stability in a static system is by limiting variety. This makes static systems authoritarian.
+  
+  Even then, the environment will continue changing.
+  
+  By contrast, living systems are dynamic. They coevolve with the environment, and with the agents that make them up.
+  
+  Instead of limiting the variety of people, increase the variety of the system.`,
+    author: "gordonbrander",
+  },
+  {
     title: "Beauty, Order, Chaos",
     body: `Perhaps the reason organic patterns are so beautiful is that they straddle the line between order and chaos. we can see there is a pattern, but we cannot directly see that pattern. it’s about reaching the limits of our perception, the reason constraints breed creativity is they constrain the chaos enough to spot a little more order, which we can incorporate and then relax the constraints again. `,
     author: "bfollington",
